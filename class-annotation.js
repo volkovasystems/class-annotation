3dbe89219a43cf877729993ad47cd12d59fd0ade
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-08-20 14:01:19 +0800

    Initial commit with class-annotation.js.

---
+++ class-annotation.js
@@ -0,0 +1,12 @@
+var Annotation = function Annotation( value ){
+
+};
+
+Annotation.prototype.append = function append( annotation, operator ){
+
+};
+
+Annotation.prototype.prepend = function prepend( annotation, operator ){
+
+};
+
